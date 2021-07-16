cc3cef952ab8200f4f3dfc12202fac05eb6352cd
Julien Serrurier <contact@julienserrurier.fr>
2021-07-16 10:54:17 +0200

    feat: change app name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,12 +5,8 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -20,16 +16,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ng-localize-runtime'`, () => {
+  it(`should have as title 'i18n demo'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ng-localize-runtime');
+    expect(app.title).toEqual('i18n demo');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ng-localize-runtime app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain(
+      'i18n demo app is running!'
+    );
   });
 });
